0613dd630fcda287ec230b30104be720a9ac547f
chrisbriz <christopher.brizet@outlook.com>
2022-02-04 19:25:08 +0100

    Add appcontext, reducer and business logic

--- board/src/App.tsx
+++ board/src/App.tsx
@@ -2,19 +2,20 @@ import {AppContainer} from "./styles";
 import { Column } from "./components/column-container/column-container";
 import { AddNewItem } from "./components/add-item/add-item";
 import { useAppState } from "./state/AppStateContext";
+import { addList } from "./state/actions"
 
 export const App = () => {
-const {lists} = useAppState()
+  const { lists, dispatch } = useAppState()
 
   return (
     <AppContainer>
       {lists.map((list) => (
-        <Column text={list.text} key={list.id} id={list.id}/>
+        <Column text={list.text} key={list.id} id={list.id} />
       ))}
       <AddNewItem
         toggleButtonText="+ Add another list"
-        onAdd={console.log}
+        onAdd={(text) => dispatch(addList(text))}
       />
     </AppContainer>
-  );
-};
+  )
+}
--- board/src/components/column-container/column-container.tsx
+++ board/src/components/column-container/column-container.tsx
@@ -1,27 +1,27 @@
 import { ColumnContainer, ColumnTitle } from "../../styles";
 import { Card } from "../card-container/card-container";
 import { AddNewItem } from "../add-item/add-item";
-import { useAppState } from "../../state/AppStateContext"
+import { useAppState } from "../../state/AppStateContext";
+import { addTask } from "../../state/actions";
 
 type ColumnProps = {
   text: string;
-  id: string
+  id: string;
 };
 
 export const Column = ({ text, id }: ColumnProps) => {
-  const {getTasksByListId} = useAppState()
-
-  const tasks = getTasksByListId(id)
+  const { getTasksByListId, dispatch } = useAppState();
+  const tasks = getTasksByListId(id);
 
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-        {tasks.map((task) => (
-          <Card text={task.text} key={task.id} id={task.id}/>
-        ))}
+      {tasks.map((task) => (
+        <Card text={task.text} key={task.id} id={task.id} />
+      ))}
       <AddNewItem
         toggleButtonText="+ Add another card"
-        onAdd={console.log}
+        onAdd={(text) => dispatch(addTask(text, id))}
         dark
       />
     </ColumnContainer>
