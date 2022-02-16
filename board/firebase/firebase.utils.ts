33b1c39c40b27d7134ad7b208d22962998ad6201
chrisbriz <christopher.brizet@outlook.com>
2022-02-16 06:57:09 +0100

    Added cloudflare

---
+++ board/firebase/firebase.utils.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6lw7uZNwhf5QK5vGWC7trFjgkoSdfoZU",
+  authDomain: "examen-73662.firebaseapp.com",
+  projectId: "examen-73662",
+  storageBucket: "examen-73662.appspot.com",
+  messagingSenderId: "949050720337",
+  appId: "1:949050720337:web:fcae55715449a48e247870",
+  measurementId: "G-FEQHTPBGHN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
